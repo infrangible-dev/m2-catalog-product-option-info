77161125e1c1f8a5fd59730de556af1fd83ec4b6
Andreas Knollmann <a.knollmann@seak.dev>
2025-06-18 12:23:32 +0200

    fix double label

--- src/Plugin/Catalog/Ui/DataProvider/Product/Form/Modifier/CustomOptions.php
+++ src/Plugin/Catalog/Ui/DataProvider/Product/Form/Modifier/CustomOptions.php
@@ -194,17 +194,18 @@ class CustomOptions
             'arguments' => [
                 'data' => [
                     'config' => [
-                        'label'          => __('Info Image'),
-                        'componentType'  => Field::NAME,
-                        'dataScope'      => static::FIELD_IMAGE_NAME,
-                        'dataType'       => Text::NAME,
-                        'sortOrder'      => $sortOrder,
-                        'formElement'    => 'imageUploader',
-                        'elementTmpl'    => 'ui/form/element/uploader/uploader',
-                        'previewTmpl'    => 'Magento_Catalog/image-preview',
-                        'uploaderConfig' => [
+                        'label'             => __('Info Image'),
+                        'componentType'     => Field::NAME,
+                        'dataScope'         => static::FIELD_IMAGE_NAME,
+                        'dataType'          => Text::NAME,
+                        'sortOrder'         => $sortOrder,
+                        'formElement'       => 'imageUploader',
+                        'elementTmpl'       => 'ui/form/element/uploader/uploader',
+                        'previewTmpl'       => 'Magento_Catalog/image-preview',
+                        'uploaderConfig'    => [
                             'url' => 'product_option_info/image/upload'
-                        ]
+                        ],
+                        'additionalClasses' => 'catalog-product-option-info-image'
                     ]
                 ]
             ]
